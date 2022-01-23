044b0a9da7bfbd247600537f27a7478798733f90
shweta gaikwad <shwetagaikwad274@gmail.com>
2022-01-23 14:10:06 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace call_by_value
+{
+    class Program
+
+    {
+        public void show(int val)
+        {
+            val *= val;
+            Console.WriteLine("val inside calling function" + val);
+
+        }
+        static void Main(string[] args)
+
+        {
+           int  Val = 50;
+            Program p = new Program();
+            Console.WriteLine("Value before calling the function " + val);
+            Program.show(val);
+            Console.WriteLine("value after calling function" + val);
+
+        }
+            
+        
+    }
+}
